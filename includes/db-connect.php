b68ee1e39cf56d0a508d367c13246c609c09bc51
trblandford4@gmail.com <trblandford4@gmail.com>
2017-04-05 15:31:36 -0400

    Update connection string for ClearDB

--- includes/db-connect.php
+++ includes/db-connect.php
@@ -1,6 +1,21 @@
 <?php
     function connectDB() {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+        //localhost
+//        $server = 'localhost';
+//        $username = 'student';
+//        $password = 'student';
+//        $db = 'meggles';
+
+
+        $server = $url["host"];
+        $username = $url["user"];
+        $password = $url["pass"];
+        $database = substr($url["path"], 1);
+
+
         global $db;  // makes this variable available outside of just the scope of this function
-        $db = mysqli_connect('localhost', 'student', 'student', 'meggles') or die ("I cannot connect to the database because: " . mysqli_connect_error());
+        $db = mysqli_connect($server, $username, $password, $database) or die ("I cannot connect to the database because: " . mysqli_connect_error());
     }
 ?>
